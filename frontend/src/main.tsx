725a986833b880a3e16b90b7382d9fe5601428e2
royg24 <roy@goldhar.net>
2025-08-26 12:43:43 +0300

    Tasks context added

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import AppProvider from "@/AppProvider.tsx";
+import {TasksProvider} from "@/contexts/TasksContext.tsx";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
       <AppProvider>
-        <App />
+          <TasksProvider>
+            <App />
+          </TasksProvider>
       </AppProvider>
   </StrictMode>,
 )
